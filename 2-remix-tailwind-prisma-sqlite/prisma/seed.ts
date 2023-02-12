bd46a9dcecf95082e5fda5f8d7b903825ea8c658
Leandro Fernandez <leandroofernandezz@gmail.com>
2023-02-12 11:52:39 -0300

    add prisma schema

--- 2-remix-tailwind-prisma-sqlite/prisma/seed.ts
+++ 2-remix-tailwind-prisma-sqlite/prisma/seed.ts
@@ -1,53 +1,188 @@
-import { PrismaClient } from "@prisma/client";
-import bcrypt from "bcryptjs";
-
+import { Prisma, PrismaClient, User, Room, Message } from "@prisma/client";
+import { faker } from "@faker-js/faker";
 const prisma = new PrismaClient();
 
-async function seed() {
-  const email = "rachel@remix.run";
+async function main() {
+  const usersSeed: Prisma.UserCreateInput[] = [
+    {
+      name: faker.name.fullName(),
+      email: faker.internet.email(),
+    },
+    {
+      name: faker.name.fullName(),
+      email: faker.internet.email(),
+    },
+    {
+      name: faker.name.fullName(),
+      email: faker.internet.email(),
+    },
+    {
+      name: faker.name.fullName(),
+      email: faker.internet.email(),
+    },
+    {
+      name: faker.name.fullName(),
+      email: faker.internet.email(),
+    },
+  ];
 
-  // cleanup the existing database
-  await prisma.user.delete({ where: { email } }).catch(() => {
-    // no worries if it doesn't exist yet
-  });
+  // upsert users
+  let users: User[] = [];
+  for (let i = 0; i < usersSeed.length; i++) {
+    users[i] = await prisma.user.upsert({
+      where: { email: usersSeed[i].email },
+      update: {},
+      create: {
+        email: usersSeed[i].email,
+        name: usersSeed[i].name,
+      },
+    });
+  }
 
-  const hashedPassword = await bcrypt.hash("racheliscool", 10);
+  const [user1, user2, user3, user4, user5] = users;
 
-  const user = await prisma.user.create({
-    data: {
-      email,
-      password: {
-        create: {
-          hash: hashedPassword,
-        },
+  const roomsSeed: Array<Prisma.RoomCreateInput & { users: number[] }> = [
+    {
+      name: "general",
+      users: users.map((user) => user.id),
+    },
+    {
+      name: "random",
+      users: [user1.id, user2.id, user3.id],
+    },
+    {
+      name: "jokes",
+      users: [user3.id, user4.id, user5.id],
+    },
+    {
+      name: "memes",
+      users: [user1.id, user2.id, user5.id],
+    },
+  ];
+
+  // upsert rooms
+  let rooms: Room[] = [];
+  for (let i = 0; i < roomsSeed.length; i++) {
+    rooms[i] = await prisma.room.create({
+      data: {
+        name: roomsSeed[i].name,
+      },
+    });
+  }
+
+  const [generalRoom, randomRoom, jokesRoom, memesRoom] = rooms;
+
+  const messagesSeed: Array<Prisma.MessageCreateInput> = [
+    {
+      text: "Welcome to General!",
+      author: {
+        connect: { id: user1.id },
+      },
+      room: {
+        connect: { id: generalRoom.id },
       },
     },
-  });
-
-  await prisma.note.create({
-    data: {
-      title: "My first note",
-      body: "Hello, world!",
-      userId: user.id,
+    {
+      text: "Here we post serious stuff",
+      author: {
+        connect: { id: user2.id },
+      },
+      room: {
+        connect: { id: generalRoom.id },
+      },
     },
-  });
-
-  await prisma.note.create({
-    data: {
-      title: "My second note",
-      body: "Hello, world!",
-      userId: user.id,
+    {
+      text: "Welcome to Random!",
+      author: {
+        connect: { id: user1.id },
+      },
+      room: {
+        connect: { id: randomRoom.id },
+      },
     },
-  });
+    {
+      text: "Here we post random stuff",
+      author: {
+        connect: { id: user3.id },
+      },
+      room: {
+        connect: { id: randomRoom.id },
+      },
+    },
+    {
+      text: "Welcome to Jokes!",
+      author: {
+        connect: { id: user3.id },
+      },
+      room: {
+        connect: { id: jokesRoom.id },
+      },
+    },
+    {
+      text: "Here we post jokes",
+      author: {
+        connect: { id: user4.id },
+      },
+      room: {
+        connect: { id: jokesRoom.id },
+      },
+    },
+    {
+      text: "Welcome to Memes!",
+      author: {
+        connect: { id: user1.id },
+      },
+      room: {
+        connect: { id: memesRoom.id },
+      },
+    },
+    {
+      text: "Here we post memes",
+      author: {
+        connect: { id: user5.id },
+      },
+      room: {
+        connect: { id: memesRoom.id },
+      },
+    },
+  ];
 
-  console.log(`Database has been seeded. 🌱`);
+  // upsert messages
+  let messages: Message[] = [];
+  for (let i = 0; i < messagesSeed.length; i++) {
+    await prisma.message.create({
+      data: messagesSeed[i],
+    });
+  }
+
+  // connect users to rooms
+  for (let i = 0; i < roomsSeed.length; i++) {
+    for (let j = 0; j < roomsSeed[i].users.length; j++) {
+      await prisma.userOnRoom.create({
+        data: {
+          user: {
+            connect: {
+              id: roomsSeed[i].users[j],
+            },
+          },
+          room: {
+            connect: {
+              id: rooms[i].id,
+            },
+          },
+        },
+      });
+    }
+  }
+
+  console.log({ users, rooms, messages });
 }
-
-seed()
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  })
-  .finally(async () => {
+main()
+  .then(async () => {
     await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
   });
